b38427360e4798822bcca7fadc096c3b1377055e
Betty <bettyvuong13@gmail.com>
2026-01-05 18:03:25 -0500

    Changes made for deployment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #rough idea of an entry point for a Flask application
+import os
 from src import create_app
 
 app = create_app()
@@ -8,4 +9,6 @@ app = create_app()
 #     db.create_all()  # Create database tables for our data models
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get("FLASK_DEBUG", "false").lower() == "true"
+    app.run(host='0.0.0.0', port=port, debug = debug)
